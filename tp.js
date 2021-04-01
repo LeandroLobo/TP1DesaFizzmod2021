2728bd1722ac200750d289d46d4035dbfe522647
LeandroLobo <leandrolobomarquez@gmail.com>
2021-03-31 21:24:36 -0300

    la función CrearClase() tiene errores en el test, pero son errores supuestos de sintaxis del código, aunque la función se ejecuta correctamente

--- tp.js
+++ tp.js
@@ -14,14 +14,15 @@ function contarVocales (texto) {
 // De no haber coincidencias devuelve null, lo que significa que la cantidad de vocales es === 0.
 // Previamente se eliminó la posibilidad de que el parametro no sea del tipo string
 
-// console.log(`
-//   function ContarVocales():
-//   "Hola mundo" returns: ${contarVocales("Hola mundo")},
-//   "HOlA mundó" returns: ${contarVocales("HOlA mundó")},
-//   "Hl mnd" returns: ${contarVocales("Hl mnd")},
-//   "": ${contarVocales("")},
-//   null returns: ${contarVocales(null)}
-// `);
+console.log(`
+  function ContarVocales():
+  "Hola mundo" returns: ${contarVocales("Hola mundo")},
+  "HOlA mundó" returns: ${contarVocales("HOlA mundó")},
+  "Hl mnd" returns: ${contarVocales("Hl mnd")},
+  "": ${contarVocales("")},
+  null returns: ${contarVocales(null)}
+`);
+
 
 /* 
 Crear un repo en github y subir todo el proyecto. 
@@ -30,13 +31,32 @@ Esta función devolverá un string con la url del repo.
 */
 const urlRepo = () => 'https://github.com/LeandroLobo/TP1DesaFizzmod2021.git';
 
+
 /* 
-Crear una función arrow, que devuelva una clase en ES6 que contendrá dos métodos llamados contadorPalabras y hayNumeros. La clase recibirá un texto que se guardará en una propiedad llamada texto. contadorPalabras retornará la cantidad de palabras encontradas en la propiedad texto y hayNumeros devolverá true en caso de que encuentre un número en dicho texto, caso contrario retorna false. En ambos métodos, si el texto no es válido, se devolverá -1
+Crear una función arrow, que devuelva una clase en ES6 que contendrá dos métodos llamados contadorPalabras y hayNumeros.
+La clase recibirá un texto que se guardará en una propiedad llamada texto.
+contadorPalabras retornará la cantidad de palabras encontradas en la propiedad texto. 
+hayNumeros devolverá true en caso de que encuentre un número en dicho texto, caso contrario retorna false.
+En ambos métodos, si el texto no es válido, se devolverá -1
 Crear un propiedad estática contadorInstancias que me indique cuantas instancias hay de esa clase.
 */
-const crearClase = () => {
-  return
+const crearClase = () => class Retorno {
+
+  static contadorInstancias = 0;
+  
+  constructor(texto = '') {
+    this.texto = texto;
+    Retorno.contadorInstancias += 1;
+  }
+
 }
+
+const Clase = crearClase()
+console.log(typeof Clase.contadorInstancias, Clase.contadorInstancias)
+const objeto = new Clase('perro que ladra no muerde');
+console.log(typeof Clase.contadorInstancias, Clase.contadorInstancias)
+console.log(objeto.texto)
+
 
 module.exports = {
   contarVocales,
